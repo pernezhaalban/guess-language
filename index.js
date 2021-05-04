3511e3e31de51ab31e5afca67a84b98ec534bf38
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-05-05 00:16:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const words = process.argv[2];
+const langs = require('langs');
+const franc = require('franc');
+const colors = require('colors');
+
+const fromFranc = franc(words);
+if (fromFranc == 'und') {
+    console.log('sorry, couldnt guess the language! try again with some more sample text!'.red);
+} else {
+    const detected = langs.where("3", fromFranc);
+    console.log(`Our best guess is: ${detected.name}`.pink);
+}
